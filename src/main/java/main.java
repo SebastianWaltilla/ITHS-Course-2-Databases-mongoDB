909167976279d6cdc9b3ee7242c740c3b0374c7f
sebas <sebastianwaltilla@hotmail.com>
2020-01-28 16:27:08 +0100

    now functional mvn and ability to create a database in mongo

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,56 @@
+package main.java;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonValue;
+import javax.json.JsonObject;
+import static javax.json.JsonValue.ValueType.*;
+import javax.json.JsonReader;
+import javax.json.JsonStructure;
+
+
 public class main {
 
+    public static void main(String[] args) {
 
-    //test to add a line
+
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+
+        // Create database
+        MongoDatabase database = mongo.getDatabase("lab3");
+        System.out.println("made lab3 database");
+
+        //Create collection
+        database.createCollection("restaurants");
+        System.out.println("collection restaurants created successfully");
+
+
+
+
+
+
+
+        //
+        database.getCollection("resturants").drop();
+
+
+
+
+
+
+
+
+    }
+
+
+
+
 
 }
 
