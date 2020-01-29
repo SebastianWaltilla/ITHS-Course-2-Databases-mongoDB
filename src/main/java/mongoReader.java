c6cb187051c327c3aa8905ac116b7d20b75e4f61
sebas <sebastianwaltilla@hotmail.com>
2020-01-29 12:17:28 +0100

    finaly get multiple prints from different documents, real deal!

--- src/main/java/mongoReader.java
+++ src/main/java/mongoReader.java
@@ -18,39 +18,44 @@ public class mongoReader {
     // }
 
     public void parseAndPrint()throws FileNotFoundException {
-        try{
+
+        try {
             JsonReader reader = Json.createReader(new FileReader("C:\\Users\\sebas\\IdeaProjects\\MongoDBJava\\src\\main\\resources\\cafe"));
             JsonStructure jsonStruct = reader.read();
 
-            if(jsonStruct.getValueType().equals(OBJECT)){
+            if (jsonStruct.getValueType().equals(OBJECT)) {
+
                 JsonObject jo = (JsonObject) jsonStruct;
-                System.out.println("Casting to JsonObject...");
+                JsonArray doc = jo.getJsonArray("document");
 
-                System.out.println("_id: " + jo.getString("_id", "not working"));
-                System.out.println("name: " + jo.getString("name", "not working"));
-                System.out.println("stars: " + jo.getString("stars", "not working"));
+                for (int i = 0; i < doc.size(); i++) {
 
+                    System.out.println("_id: " + doc.getJsonObject(i).getString("_id", "piss"));
+                 /*
+                   System.out.println("name: " + jo.getString("name", "not working"));
+                   System.out.println("stars: " + jo.getString("stars", "not working"));
 
-                JsonArray arr = jo.getJsonArray("categories");
-
-
-                System.out.print("Categories:");
-                for (int i = 0; i < arr.size() ; i++) {
-                    System.out.print(", " + arr.getString(i));
+                    JsonArray arr = jo.getJsonArray("categories");
+                    System.out.print("Categories: [");
+                    for (int i = 0; i < arr.size() ; i++) {
+                        System.out.print(arr.getString(i));
+                        if (i != arr.size()-1){
+                            System.out.print(", ");
+                        }
+                    }
+                    System.out.print("]");
+*/
                 }
 
-
-
-
-
             }
-        }catch(FileNotFoundException fnfe){
+
+        } catch (FileNotFoundException fnfe) {
             throw new FileNotFoundException("jsonFile");
         }
 
     }
-
-
-
-
 }
+
+
+
+
